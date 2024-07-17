ceeb08140ff7e7f4a2c20b1dfe7b759fbbd16079
Artur Markouski <markouski.artur@student.ehu.lt>
2024-07-17 14:20:39 +0200

    feat: add solving 4 tasks

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,30 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        if (multiplyByAndToInclusive == 0){
+            return;
+        } else if (multiplyByAndToInclusive > 0) {
+            int i = 0;
+            while (i <= multiplyByAndToInclusive){
+                int multi = i;
+                multi *= multiplyByAndToInclusive;
+                System.out.println(multi);
+                i++;
+            }
+        }else {
+            int length = multiplyByAndToInclusive * -1;
+            int i = 0;
+            while (i <= length){
+                int multi = i;
+                multi *= multiplyByAndToInclusive;
+                System.out.println(multi);
+                i++;
+             }
+        }
+    }
+
+    public static void main(String[] args) {
+        Multiplication multiplication = new Multiplication();
+        multiplication.printMultiplied(-5);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int value = 2;
+        while (value <= printToInclusive){
+            boolean isPrime = true;
+            int divisor = 2;
+            while (divisor <= value / 2) {
+                if (value % divisor == 0) {
+                    isPrime = false;
+                    break;
+                }
+                divisor++;
+            }
+            if (isPrime) {
+                System.out.println(value);
+            }
+            value++;
+        }
     }
 }
