4f5e98fd4b2a97a92c71b7af2d219261fbd0c308
Artur Markouski <markouski.artur@student.ehu.lt>
2024-07-17 16:42:57 +0200

    feat: add solving all tasks

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        if (power < 0){
+            System.out.println("too much power");
+        }
+        int i = 0;
+        int result;
+        while (i <= power){
+            result = (int)(Math.pow(2,i));
+            System.out.println(result);
+            i++;
+        }
 
     }
 }
